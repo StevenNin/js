d2c639caa41f2008efdd5008bff275cb7c0b4ee8
邹鹏steven <401771873@qq.com>
2021-10-19 17:55:44 +0800

    nothing else

---
+++ cars-screen/src/components/centerLeft/centerLeft1Chart/chart.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <Echart
+        :options="options"
+        id="centerLeft1Chart"
+        height="220px"
+        width="260px"
+        ></Echart>
+    </div>
+</template>
+
+<script>
+    import Echart from "@/common/echart";
+export default {
+    data() {
+        return{
+            options: {}
+        }
+    },
+    components: {
+        Echart,
+    },
+    props: {
+        cdata:{
+            type: Object,
+            default: ()=>{}
+        },
+    },
+    watch: {
+        cdata: {
+            handler (newData) {
+                this.options = {
+                    color:[
+
+                    ],
+                    tooltip: {
+                        trigger: "item",
+                        formatter: "{a} <br/> {b} : {c} ({d}%)"
+                    },
+                    toolbox: {
+                        show:true
+                    },
+                }
+            }
+        }
+    }
+}
+</script>
