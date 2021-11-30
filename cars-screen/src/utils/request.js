46a9ecf6819cfc69e99b25a41d93001e8bf3554c
邹鹏steven <401771873@qq.com>
2021-11-30 23:39:21 +0800

    单页面适配模板

--- cars-screen/src/utils/request.js
+++ cars-screen/src/utils/request.js
@@ -4,4 +4,24 @@ const service = axios.create({
     baseURL:'process.env.VUE_APP_BASE_API',
     timeout: 120000,
 })
+service.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded'
+
+// 拦截器
+service.interceptors.request.use(function(config){
+    return config
+},function(error){
+    return Promise.reject(error)
+})
+
+service.interceptors.response.use(function(config){
+    return config
+},function(error){
+    return Promise.reject(error)
+})
+
+// 取消拦截
+
+
+
+
 export default service
